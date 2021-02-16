5044134cb2f80826431394c6c330d43901eca2e6
vinodha kumara L <vinodhakumara2681997@gmail.com>
2021-02-16 23:15:07 +0530

    Fixing a bug

--- UDP Group Chat/chat.py
+++ UDP Group Chat/chat.py
@@ -18,9 +18,9 @@ def sendToAll1():  # This will sends a message to mentioned OS
         # Give a OS IP in string  and PORT in digit numbers
         # Example s.sendto( "message".encode(), ( "IP", PORT_Number )
         # message should be encoded(message should be BYTE TYPE) due to network can only recieve byte type
-        s.sendto("{}".format(x).encode(), ("172.17.0.2", 2222))
-        s.sendto("{}".format(x).encode(), ("192.168.43.254", 1234))
-        s.sendto("{}".format(x).encode(), ("192.168.43.89", 1234))
+        s.sendto("{}".format(message).encode(), ("172.17.0.2", 2222))
+        s.sendto("{}".format(message).encode(), ("192.168.43.254", 1234))
+        s.sendto("{}".format(message).encode(), ("192.168.43.89", 1234))
                 
 # Sends Message If first thread is busy this helps like can sends multiple images or chats if one takes time to load than rest messages will be  sending
 # Optional Extra Added
@@ -33,9 +33,9 @@ def sendToAll2():
         # Give a OS IP in string  and PORT in digit numbers
         # Example s.sendto( "message".encode(), ( "IP", PORT_Number )
         # message should be encoded(message should be BYTE TYPE) due to network can only recieve byte type
-        s.sendto("{}".format(x).encode(), ("192.168.43.254", 32768))
-        s.sendto("{}".format(x).encode(), ("172.17.0.2", 2222))
-        s.sendto("{}".format(x).encode(), ("192.168.43.89", 1234))
+        s.sendto("{}".format(message).encode(), ("192.168.43.254", 32768))
+        s.sendto("{}".format(message).encode(), ("172.17.0.2", 2222))
+        s.sendto("{}".format(message).encode(), ("192.168.43.89", 1234))
         
 def recieve():   # Recieves Message
     # Create a socket Format like Netwrk protocol and family
@@ -45,10 +45,10 @@ def recieve():   # Recieves Message
     port = 1111
     s.bind(( ip, port ))    # Bind(Connect Port to IP) IP and PORT
     while True:
-        x = s.recvfrom(1024)        # Recieves message of  max 2MiB(1024Bytes) of message
+        message = s.recvfrom(1024)        # Recieves message of  max 2MiB(1024Bytes) of message
         # x[1][0] => IP of message Sender( even we can give name instead of IP)
         # x[0] => Message sent by Client (needs to be decoded due to client send in byte type so needs to convert orginal format)
-        msg = "{}Message from \U0001F464 {} \n{}{}".format("\t"*5,x[1][0],"\t"*5,x[0].decode())
+        msg = "{}Message from \U0001F464 {} \n{}{}".format("\t"*5,message[1][0],"\t"*5,message[0].decode())
         # Printing Message (Even we can return this to web page n can make a API chat app)
         print(msg)
         
